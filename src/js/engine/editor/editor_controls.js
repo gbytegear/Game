032d98119c0e457521e47c842369722c27ff2e17
sh13max <shamanmax13@yandex.ru>
2019-07-19 15:41:14 +0300

     

--- src/js/engine/editor/editor_controls.js
+++ src/js/engine/editor/editor_controls.js
@@ -30,6 +30,7 @@ document.addEventListener('keydown', e => {
 });
 
 document.addEventListener('keyup', e => {
+    if(e.target != document.body)return;
     if (e.keyCode === 38 || e.keyCode === 87 || e.keyCode === 38) keys.up = false;
     if (e.keyCode === 39 || e.keyCode === 68 || e.keyCode === 39) keys.right = false;
     if (e.keyCode === 40 || e.keyCode === 83 || e.keyCode === 40) keys.down = false;
@@ -59,27 +60,127 @@ editor.loop.insertBackFunction(() => {
 
 let inf = document.querySelector('.info'),
 editor_settings = {
-    delta: 1,
+    delta: .1,
 }
 
 canvas.addEventListener('mousemove', e => {
     inf.style.left = e.clientX + 5 + 'px';
     inf.style.top = e.clientY + 'px';
-    if(keys.selecting)editor.selection.size = [(e.clientX - editor.map.position[0] - canvas.width/2) - editor.selection.position[0], (e.clientY - editor.map.position[1] - canvas.height/2) - editor.selection.position[1]];
+    if(keys.selecting)
+        editor.selection.size = [
+            Math.round((
+                (e.clientX - editor.map.position[0] - canvas.width/2) - editor.selection.position[0]
+                ) / (editor_settings.delta*10)) * (editor_settings.delta*10),
+            Math.round((
+                (e.clientY - editor.map.position[1] - canvas.height/2) - editor.selection.position[1]
+                ) / (editor_settings.delta*10)) * (editor_settings.delta*10)
+        ];
 
-    inf.innerText = `Mouse:(x:${(e.clientX - editor.map.position[0] - canvas.width/2) }; y:${(e.clientY - editor.map.position[1] - canvas.height/2) })
+    inf.innerText = `Mouse:{x:${(e.clientX - editor.map.position[0] - canvas.width/2) }; y:${(e.clientY - editor.map.position[1] - canvas.height/2) }}
         Delta:${editor_settings.delta}
         ${keys.selecting?`Selected:(pos:(${JSON.stringify(editor.selection.position)}); size:(${JSON.stringify(editor.selection.size)}))`:''}
     `;
 });
 
 canvas.addEventListener('mousedown', e => {
-    editor.selection.position = [e.clientX - editor.map.position[0] - canvas.width/2, e.clientY - editor.map.position[1] - canvas.height/2];
+    editor.selection.position = [
+        Math.round((e.clientX - editor.map.position[0] - canvas.width/2) / (editor_settings.delta*10)) * (editor_settings.delta*10),
+        Math.round((e.clientY - editor.map.position[1] - canvas.height/2) / (editor_settings.delta*10)) * (editor_settings.delta*10)
+    ];
     editor.selection.size = [0,0];
     keys.selecting = true;
 });
 
 canvas.addEventListener('mouseup', e => {
     keys.selecting = false;
-    // editor.selection.size = [e.clientX - editor.selection.position[0], e.clientY - editor.selection.position[1]];
+    editor.selection.size = [
+        Math.round(((e.clientX - editor.map.position[0] - canvas.width/2) - editor.selection.position[0]) / (editor_settings.delta*10)) * (editor_settings.delta*10),
+        Math.round(((e.clientY - editor.map.position[1] - canvas.height/2) - editor.selection.position[1]) / (editor_settings.delta*10)) * (editor_settings.delta*10)
+    ];
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CLI
+HTMLDataListElement.prototype.containsValue = function(value){
+    for(let option of this.children)
+        if(option.value == value)return true;
+    return false;
+}
+
+const cli = new class CommandLineinterface {
+    constructor(){
+        Object.defineProperties(this, {
+            addObject:{
+                value: async function(to, properties = {}){
+                    properties.position = editor.selection.position;
+                    properties.size = editor.selection.size;
+                    editor.map_json[to].push(properties);
+                    editor.map.load(editor.map_json);
+                    document.querySelector('json-editor').reload();
+                },
+                enumerable: true,
+                writable: false
+            },
+            addTiles:{
+                value: async function(src = 'transparent'){
+                    let tile = new Object;
+                    tile.from = [editor.selection.position[0] / 100, editor.selection.position[1] / 100];
+                    tile.to = [editor.selection.size[0] / 100 + tile.from[0], editor.selection.size[1] / 100 + tile.from[1]];
+                    tile.src = src;
+                    editor.map_json.tiles.range_tiles.push(tile);
+                    editor.map.load(editor.map_json);
+                    document.querySelector('json-editor').reload();
+                },
+                enumerable: true,
+                writable: false
+            },
+            setDelta:{
+                value: async function(delta){editor_settings.delta = delta;},
+                enumerable: true,
+                writable: false
+            }
+        });
+
+        for(let command in this){
+            let element = document.createElement('option');
+            element.value = 'this.' + command;
+            
+            commands.appendChild(element);
+            if(typeof(this[command]) == 'function')element.value += '()';
+        }
+
+        document.querySelector('.command-line').addEventListener('keyup', e => (()=>{
+            if (e.keyCode !== 13)return;
+            try{
+                eval(e.target.value);
+                if(!commands.containsValue(e.target.value)){
+                    let element = document.createElement('option');
+                    element.value = e.target.value;
+                    commands.appendChild(element);
+                }
+                e.target.value = "";
+            }catch(err){
+                alert(err);
+                console.error(err);
+            }
+        })())
+    }
+}
