042a3dea937083a1867eb4720530d73db2aa262c
sh13max <shamanmax13@yandex.ru>
2019-07-24 17:05:20 +0300

     

--- src/js/engine/main.js
+++ src/js/engine/main.js
@@ -3,10 +3,10 @@ const ge = new class GameEngine {
         this.loop = canvas.loop;
         this.map = new MapController;
         this.buildGameSceneStructure();
+        this.playerCharacterProcessing();
         // TEST
         this.player.setTextures(data.items.clth_empty);
         this.player.setTextures(data.heads.player);
-
         this.map.load(data.maps[data.start_map]);
     }
 
@@ -19,5 +19,18 @@ const ge = new class GameEngine {
             'map_object_layer.foreground_layer'];
         canvas.query('player').anchors = {position: 'center'};
         this.player = canvas.query('player');
+        delete this.constructor.prototype.buildGameSceneStructure;
+    }
+
+    playerCharacterProcessing(){
+        Object.defineProperty(this.player.stats, 'hp', {
+            set: hp => {
+                this.player.stats._hp = hp;
+                return new Promise(()=>document.querySelector('.hitbar').style.setProperty('--hp', (this.player.stats._hp / (this.player.stats.max_hp / 100)) + "%"))
+            },
+            get: () => this.player.stats._hp
+        });
+        this.player.stats.hp = this.player.stats.max_hp;
+        delete this.constructor.prototype.playerCharacterProcessing;
     }
 }
